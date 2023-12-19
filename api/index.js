8faebbecdd7971aca98845a9ed84d45645c019cc
Marcelo Brito <marcellus.brito@gmail.com>
2023-12-18 22:53:26 -0300

    apolo4

--- api/index.js
+++ api/index.js
@@ -1,39 +1,53 @@
-import { ApolloServer, gql } from "apollo-server-express";
-import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
-import http from "http";
-import express from "express";
-import cors from "cors";
+// import { ApolloServer, gql } from "apollo-server-express";
+// import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+// import http from "http";
+// import express from "express";
+// import cors from "cors";
 
-const app = express();
+// const app = express();
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-const httpServer = http.createServer(app);
+// const httpServer = http.createServer(app);
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
+// const typeDefs = gql`
+//   type Query {
+//     hello: String
+//   }
+// `;
 
-const resolvers = {
-  Query: {
-    hello: () => "world",
-  },
-};
+// const resolvers = {
+//   Query: {
+//     hello: () => "world",
+//   },
+// };
 
-const startApolloServer = async(app, httpServer) => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
+// const startApolloServer = async(app, httpServer) => {
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+//   });
 
-  await server.start();
-  server.applyMiddleware({ app });
-}
+//   await server.start();
+//   server.applyMiddleware({ app });
+// }
 
-startApolloServer(app, httpServer);
+// startApolloServer(app, httpServer);
 
-export default httpServer;
+// export default httpServer;
+
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs, resolvers } from "./schema";
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
+  context: async ({ req }) => ({ token: req.headers.token }),
+
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at ${url}`);
